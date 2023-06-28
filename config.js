69e72d9b6be3f47372e1e2ff11f3746f7fec2370
fzf404 <me@fzf404.art>
2023-06-28 13:21:07 +0800

    feat: esm support

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
 module.exports = {
-	extends: ['eslint-config-prettier'],
+	env: {
+		es6: true,
+		browser: true,
+		node: true,
+	},
 	plugins: ['eslint-plugin-prettier'],
+	extends: ['eslint:recommended', 'eslint-config-prettier'],
 	rules: {
 		'prettier/prettier': 'error',
+	},
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
 	},
 }
