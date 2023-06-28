d52a74c13a5706f5ac2d98c998f5a015aeaf40ff
fzf404 <me@fzf404.art>
2023-06-28 15:14:06 +0800

    style: replace tab with space

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
-	env: {
-		es6: true,
-		browser: true,
-		node: true,
-	},
-	plugins: ['eslint-plugin-prettier'],
-	extends: ['eslint:recommended', 'eslint-config-prettier'],
-	rules: {
-		'prettier/prettier': 'error',
-	},
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  plugins: ['eslint-plugin-prettier'],
+  extends: ['eslint:recommended', 'eslint-config-prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 }
