e77d5589bc10a2a3e09da8e5d74062c734868a52
fzf404 <me@fzf404.art>
2023-07-08 16:08:29 +0800

    refactor: split config file

--- config.js
+++ config.js
@@ -6,31 +6,10 @@ module.exports = {
     node: true,
     browser: true,
   },
-  extends: ['eslint:recommended', './src/prettier.js', './src/vue.js'],
+  extends: ['eslint:recommended', './src/prettier.js', './src/typescript.js', './src/vue.js'],
   ignorePatterns: readGitignoreFiles({ cwd: __dirname }),
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-
-  overrides: [
-    {
-      files: ['*.ts'],
-      extends: ['plugin:@typescript-eslint/recommended'],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-    },
-    {
-      files: ['*.vue'],
-      parser: 'vue-eslint-parser',
-      extends: ['plugin:vue/recommended'],
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
-      rules: {
-        'vue/multi-word-component-names': 'off',
-      },
-    },
-  ],
 }
