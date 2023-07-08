424a53b5fb2bd0f3d7d261983f862be420e45a4e
fzf404 <me@fzf404.art>
2023-07-09 00:59:30 +0800

    feat: support prettier 2 / 3

--- src/prettier.js
+++ src/prettier.js
@@ -1,8 +1,14 @@
+const { getPackageInfoSync } = require('local-pkg')
+
+const isPrettier2 = getPackageInfoSync('prettier')?.version[0] === '2'
+
+new Error('123')
+
 /** @type {import('eslint-define-config').ESLintConfig} */
 module.exports = {
-  plugins: ['eslint-plugin-prettier'],
   extends: ['eslint-config-prettier'],
+  plugins: [isPrettier2 ? 'eslint-plugin-prettier-2' : 'eslint-plugin-prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    [isPrettier2 ? 'prettier-2/prettier' : 'prettier/prettier']: 'error',
   },
 }
