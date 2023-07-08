5a0779ef19854bef215fdc22b73b9bcdd2d8a032
fzf404 <me@fzf404.art>
2023-07-08 16:55:12 +0800

    feat: vue html tag close

--- src/vue.js
+++ src/vue.js
@@ -3,12 +3,22 @@ module.exports = {
     {
       files: ['*.vue'],
       parser: 'vue-eslint-parser',
-      extends: ['plugin:vue/recommended'],
+      extends: ['plugin:vue/vue3-recommended'],
       parserOptions: {
         parser: '@typescript-eslint/parser',
       },
       rules: {
+        'vue/no-v-html': 'off',
+        'vue/require-default-prop': 'off',
         'vue/multi-word-component-names': 'off',
+        'vue/html-self-closing': [
+          'error',
+          {
+            html: {
+              void: 'always',
+            },
+          },
+        ],
       },
     },
   ],
