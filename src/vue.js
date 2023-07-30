3f39c5eeac33228bfbfea57819ef6c416862b019
fzf404 <me@fzf404.art>
2023-07-30 23:20:13 +0800

    refactor: remove support for vue2

--- src/vue.js
+++ src/vue.js
@@ -1,16 +1,10 @@
-const { getPackageInfoSync } = require('local-pkg')
-
-const isVue2 = getPackageInfoSync('vue')?.version.startsWith('2')
-
 /** @type {import('eslint-define-config').ESLintConfig} */
 module.exports = {
   overrides: [
     {
       files: ['*.vue'],
       parser: 'vue-eslint-parser',
-      extends: [
-        isVue2 ? 'plugin:vue/recommended' : 'plugin:vue/vue3-recommended',
-      ],
+      extends: ['plugin:vue/vue3-recommended'],
       parserOptions: {
         parser: '@typescript-eslint/parser',
       },
