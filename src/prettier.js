bd5f6debc46e952816ed47d0b1ec37332bf2b8b3
fzf404 <me@fzf404.art>
2023-07-30 23:04:22 +0800

    refactor: remove prettier2 support

--- src/prettier.js
+++ src/prettier.js
@@ -1,14 +1,4 @@
-const { getPackageInfoSync } = require('local-pkg')
-
-const isPrettier2 = getPackageInfoSync('prettier')?.version.startsWith('2')
-
 /** @type {import('eslint-define-config').ESLintConfig} */
 module.exports = {
-  extends: ['eslint-config-prettier'],
-  plugins: [
-    isPrettier2 ? 'eslint-plugin-prettier-2' : 'eslint-plugin-prettier',
-  ],
-  rules: {
-    [isPrettier2 ? 'prettier-2/prettier' : 'prettier/prettier']: 'error',
-  },
+  extends: ['eslint-config-prettier', 'plugin:prettier/recommended'],
 }
