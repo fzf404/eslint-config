04685f078393a708b39a7d00d832a7677204d639
fzf404 <me@fzf404.art>
2024-06-17 18:11:59 +0800

    feat: disable some unicorn rules

--- src/javascript.js
+++ src/javascript.js
@@ -5,6 +5,8 @@ module.exports = {
     'unicorn/prefer-module': 'off', // support common module
     'unicorn/filename-case': 'off', // force file name
     'unicorn/prevent-abbreviations': 'off', // force variable name
+    'unicorn/no-await-expression-member': 'off', // access await expression
+    'unicorn/no-anonymous-default-export': 'off', // allow anonymous export
     'unicorn/no-object-as-default-parameter': 'off', // allow empty object
   },
 }
